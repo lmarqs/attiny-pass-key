9e82bc1710fb85a1f89ca2982f94451ca2477191
Lucas Marques <lucas.marques@medprevonline.com>
2025-11-21 17:44:41 -0300

    Add initial Arduino sketch with DigiKeyboard integration and default password

---
+++ attiny-pass-key.ino
@@ -0,0 +1,17 @@
+#include <DigiKeyboard.h>
+
+#ifndef ATTINY_PASS_KEY_PASSWORD
+#define ATTINY_PASS_KEY_PASSWORD "default_password"
+#endif
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  DigiKeyboard.println(ATTINY_PASS_KEY_PASSWORD);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
